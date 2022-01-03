dcef4eb93cdc006019f6b3bffbc87a421b4802b0
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-01-03 22:49:54 +0300

    Initial Commit

---
+++ CapitalCities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapitalCities
+//
+//  Created by Николай Никитин on 03.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
